d910bf844411bd8a0c35bf53ee7e1fed4f6d1746
Kristy <wr7537@gmail.com>
2020-10-02 17:19:17 +0300

    Added sorting by numbers and alphabet

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 window.onload = init;
 
+let elements = [];
+
+let number = 1;
 //document.addEventListener('click', handleClick);
 
 function init(){
     var button = document.getElementById("add");
     var del = document.getElementById("del");
+    const increaseNumbers = document.getElementById("sort1");
+    const decreaseNumbers = document.getElementById("sort2");
+    const increaseAlphabet = document.getElementById("sort3");
+    const decreaseAlphabet = document.getElementById("sort4");
     button.onclick = addElement;
-    
+    increaseNumbers.addEventListener('change', increaseNumbersFunc);
+    decreaseNumbers.addEventListener('change', decreaseNumbersFunc);
+    increaseAlphabet.addEventListener('change', increaseAlphabetFunc);
+    decreaseAlphabet.addEventListener('change', decreaseAlphabetFunc);
 }
 
 // function handleClick (event) {
@@ -28,11 +38,12 @@ function addElement() {
     	var li = document.createElement("li");
     	var del = document.createElement("input");
     	var redact = document.createElement("input");
-    	li.innerHTML = "<span class=text>" + textInput.value + "</span>";
+    	li.innerHTML = "<span class=number>" + number + "</span>" + " " + "<span class=text>" + textInput.value + "</span>";
     	ul.appendChild(li);
     	li.appendChild(del);
     	li.appendChild(redact);
-    	//alert("Добавить элемент в список?");
+    	li.setAttribute("id", number)
+    	li = {"number": number, "text": textInput.value};  	
     	textInput.value = "";
 
     	del.setAttribute("type", "button");
@@ -43,15 +54,24 @@ function addElement() {
     	redact.setAttribute("type", "button");
     	redact.setAttribute("value", "Редактировать");
     	redact.setAttribute("class", "redact");
-    	redact.onclick = redactElement;
+    	redact.onclick = editElement;
+
+    	elements.push(li);
+
+    	number = number + 1;
     }
 }
 
 function deleteElement() {
+
+	let index = this.parentElement.id - 1;
+	console.log(elements);
+	console.log(index);
+	elements.splice(index, 1);
 	this.parentElement.parentElement.removeChild(this.parentElement);
 }
 
-function redactElement() {
+function editElement() {
 	var text = this.parentElement.getElementsByClassName("text")[0];
 	var del = this.parentElement.querySelector("#del");
 	var li = this.parentElement;
@@ -77,11 +97,11 @@ function redactElement() {
 	cancel.setAttribute("value", "Отмена");
 	cancel.setAttribute("class", "cancel")
 
-	confirm.onclick = confirmRedacting;
-	cancel.onclick = cancelRedacting;
+	confirm.onclick = confirmEditing;
+	cancel.onclick = cancelEditing;
 }
 
-function cancelRedacting() {
+function cancelEditing() {
 	var li = this.parentElement;
 	var text = li.getElementsByClassName("text")[0];
 	var del = li.querySelector("#del");
@@ -99,7 +119,7 @@ function cancelRedacting() {
 	cancel.remove();
 }
 
-function confirmRedacting() {
+function confirmEditing() {
 	var li = this.parentElement;
 	var del = li.querySelector("#del");
 	var text = li.getElementsByClassName("text")[0];
@@ -117,4 +137,91 @@ function confirmRedacting() {
 	input.remove();
 	confirm.remove();
 	cancel.remove();
+}
+
+function increaseNumbersFunc() {
+	elements.sort(function(a, b) {
+		if (a.number > b.number) {
+			return 1;
+		}
+		if (a.number < b.number) {
+			return -1;
+		}
+		if (a.number == b.number){
+			return 0;
+		}
+	});
+	finalFunc();
+}
+
+function decreaseNumbersFunc() {
+	elements.sort(function(a, b) {
+		if (a.number > b.number) {
+			return -1;
+		}
+		if (a.number < b.number) {
+			return 1;
+		}
+		if (a.number == b.number){
+			return 0;
+		}
+	});
+	finalFunc();
+}
+
+function increaseAlphabetFunc() {
+	elements.sort(function(a,b) {
+		if (a.text > b.text) {
+			return 1;
+		}
+		if (a.text < b.text) {
+			return -1;
+		}
+		if (a.text == b.text) {
+			return 0;
+		}
+	});
+	finalFunc();
+}
+
+function decreaseAlphabetFunc() {
+	elements.sort(function(a,b) {
+		if (a.text > b.text) {
+			return -1;
+		}
+		if (a.text < b.text) {
+			return 1;
+		}
+		if (a.text == b.text) {
+			return 0;
+		}
+	});
+	finalFunc();
+}
+
+function finalFunc() {
+	let ul = document.getElementById("list");
+	ul.innerHTML = "";
+
+    for (let i = 0; i < elements.length; i++) {
+    	let li = document.createElement("li");
+	    let del = document.createElement("input");
+	    let redact = document.createElement("input");
+	    ul.appendChild(li);
+
+    	li.innerHTML = "<span class=number>" + elements[i].number + "</span>" + " " + "<span class=text>" + elements[i].text + "</span>";
+
+    	li.appendChild(del);
+	    li.appendChild(redact);
+
+    	del.setAttribute("type", "button");
+	    del.setAttribute("value", "X");
+	    del.setAttribute("id", "del");
+	    del.onclick = deleteElement;
+
+	    redact.setAttribute("type", "button");
+	    redact.setAttribute("value", "Редактировать");
+	    redact.setAttribute("class", "redact");
+	    redact.onclick = editElement;
+    }  	
 }
